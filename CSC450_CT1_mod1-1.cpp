d03885bc477342f01557dd47bf74bd2fce0fc80d
Aida <aida.marat@gmail.com>
2024-01-21 11:41:06 -0700

    Module1CT

---
+++ CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Standard namespace declaration
+using namespace std;
+// Main Function
+int main()
+{
+  // Standard Ouput Statement
+  cout << "Welcome to this C++ Program" << endl;
+  cout << "I have corrected all errors for this program." << endl;
+  return 0;
+}
---
+++ PersonalInfo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string firstName = "Marilyn ";
+  string lastName = "Monroe";
+  int streetNumber = 12305;
+  string streetName = " Fifth Helena Dr.";
+  string city = "Los Angeles, ";
+  string state = "CA. ";
+  int zipCode = 90049;
+
+  cout << "First Name:\t" << firstName << endl;
+  cout << "Last Name:\t" << lastName << endl;
+  cout << "Street Address:\t" << streetNumber << streetName << endl;
+  cout << "City:\t\t" << city << state << endl;
+  cout << "Zip code:\t" << zipCode << endl;
+}
